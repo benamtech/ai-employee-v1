1482faf60c9a4567fa178b710c5eb4515e4cff36
benamtech <ben@amtechai.com>
2026-07-22 04:39:37 -0400

    feat(ui-variant): export variant contract

--- mvp-build/packages/shared/src/index.ts
+++ mvp-build/packages/shared/src/index.ts
@@ -2,6 +2,7 @@ export * from "./ids.js";
 export * from "./envelope.js";
 export * from "./manifest.js";
 export * from "./employee-ui-presentation.js";
+export * from "./ui-variant.js";
 export * from "./ui-lab-preset.js";
 export * from "./ui-lab-assignment-resolution.js";
 export * from "./routes.js";
